9bb1a7980bf76293a1558a3bdf04addbea46dc9b
Chamil Udayanga <chamiludayanga28@gmail.com>
2018-06-27 03:10:22 +0530

    Added api call

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import update from 'immutability-helper';
 import _ from 'lodash';
+import axios from 'axios';
 
 import ProgressBarList from './ProgressBarList';
 import ButtonList from './ButtonList';
 
-const data = { buttons: [40, 8, -28, -30], bars: [70, 80, 86], limit: 110 };
+// const data = { buttons: [40, 8, -28, -30], bars: [70, 80, 86], limit: 110 };
 
 export default class App extends Component {
     state = {
-        barList: data.bars,
+        data: undefined,
+        limit: undefined,
+        barList: undefined,
     };
+
+    componentDidMount() {
+        axios.get('http://pb-api.herokuapp.com/bars').then(
+            response => {
+                console.log(response);
+                this.setState(prevState => ({
+                    ...prevState,
+                    barList: response.data.bars,
+                    limit: response.data.limit,
+                    data: response.data,
+                }));
+            },
+            error => {
+                console.log('error', error);
+            },
+        );
+    }
 
     changeBarValue = (barId, changeValue) => {
         const newArray = _.clone(this.state.barList);
@@ -22,16 +42,21 @@ export default class App extends Component {
     };
 
     render() {
-        return (
+        return this.state.barList ? (
             <div style={{ width: 400, margin: '0 auto', marginTop: 40 }}>
-                <ProgressBarList bars={this.state.barList} limit={data.limit} />
+                <ProgressBarList
+                    bars={this.state.barList}
+                    limit={this.state.limit}
+                />
                 <div style={{ marginTop: 20 }}>
                     <ButtonList
-                        data={data}
+                        data={this.state.data}
                         changeBarValue={this.changeBarValue}
                     />
                 </div>
             </div>
+        ) : (
+            <div>Loading...</div>
         );
     }
 }
